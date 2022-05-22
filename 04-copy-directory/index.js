43f1becd3b070858bd78256e887363d5fe89a175
Alexandra Andreeva <sandra44dev@gmail.com>
2022-05-22 16:56:27 +0400

    feat: solve 04-copy-directory task Sun May 22 16:56:27     2022

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
 
+const originalFolderPath = path.join(__dirname, 'files');
+const copiedFolder = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  /* create files-copy folder */
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  fs.readdir(originalFolderPath, (err, files) => {
+
+    if (err)
+      console.log(err);
+
+    else {
+
+      files.forEach(function (file) {
+        let originalFilePath = path.join(originalFolderPath, file);
+        let copiedFile = path.join(copiedFolder, file);
+
+        function callback(err) {
+          if (err) throw err;
+        }
+
+        fs.copyFile(originalFilePath, copiedFile, callback);
+
+      });
+    }
+  });
+}
+copyDir();
