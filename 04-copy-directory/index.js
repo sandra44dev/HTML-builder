3e70a2865dde4456fd53b7f6762c53eaaf542740
Alexandra Andreeva <sandra44dev@gmail.com>
2022-05-25 23:22:24 +0400

    fix: update files when delete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,31 +6,47 @@ const copiedFolder = path.join(__dirname, 'files-copy');
 
 function copyDir() {
   /* create files-copy folder */
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  fs.mkdir(copiedFolder, { recursive: true }, (err) => {
     if (err) {
       console.log(err);
     }
-  });
+    fs.readdir(copiedFolder, (err, files) => {
+      if (err) console.log(err);
 
-  fs.readdir(originalFolderPath, (err, files) => {
-
-    if (err)
-      console.log(err);
-
-    else {
-
-      files.forEach(function (file) {
-        let originalFilePath = path.join(originalFolderPath, file);
-        let copiedFile = path.join(copiedFolder, file);
-
-        function callback(err) {
-          if (err) throw err;
-        }
-
-        fs.copyFile(originalFilePath, copiedFile, callback);
+      files.forEach(function (item) {
+        fs.unlink(path.join(copiedFolder, item), (err) => {
+          if (err) {
+            console.log('');
+          }
+        });
 
       });
-    }
+    });
+
+    fs.readdir(originalFolderPath, (err, files) => {
+
+      if (err)
+        console.log(err);
+
+      else {
+
+        files.forEach(function (file) {
+          let originalFilePath = path.join(originalFolderPath, file);
+          let copiedFile = path.join(copiedFolder, file);
+
+          function callback(err) {
+            if (err) throw err;
+          }
+
+          fs.copyFile(originalFilePath, copiedFile, callback);
+
+        });
+      }
+    });
   });
+
+
+
+
 }
 copyDir();
